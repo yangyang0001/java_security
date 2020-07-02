21deb246e7d0748ea23fc81eca8238076ed49268
yangjianwei <2017585616@qq.com>
2020-07-03 01:12:44 +0800

    MessageDigest和DigestStream应用

--- src/main/java/com/deepblue/security_002_messagedigest/MessageDigestTest.java
+++ src/main/java/com/deepblue/security_002_messagedigest/MessageDigestTest.java
@@ -7,27 +7,26 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 
-/**
- * MessageDigest 第一个引擎类, 摘要引擎类
- */
 public class MessageDigestTest {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance(AlgorithmConstant.MD5.name());
-        byte[] bytes = "MD5".getBytes();
-        messageDigest.update(bytes);
 
-        byte[] digestBytes = messageDigest.digest();
-        int digestLength = messageDigest.getDigestLength();
-        String algorithm = messageDigest.getAlgorithm();
-        Provider provider = messageDigest.getProvider();
+        MessageDigest digest = MessageDigest.getInstance(AlgorithmConstant.MD5.name());
+        byte[] bytes = "".getBytes();
+        digest.update(bytes);
 
-        // bytes        : "TUQ1", digestBytes : "fxOKCRabJQ6dyzeBQJBzeA=="
-        System.out.println("bytes        : " + JSON.toJSONString(bytes) + ", digestBytes : " + JSON.toJSONString(digestBytes));
-        System.out.println("digestLength : " + digestLength);
-        System.out.println("algorithm    : " + algorithm);
-        System.out.println("name : " + provider.getName() + ", version: " + provider.getVersion() + ", info: " + provider.getInfo());
+        int digestLength = digest.getDigestLength();
+        String algorithm = digest.getAlgorithm();
+        byte[] digestValue = digest.digest();
+        Provider provider = digest.getProvider();
 
+        System.out.println("digestLength    : " + digestLength);
+        System.out.println("algorithm       : " + algorithm);
+        System.out.println("digestValue     : " + JSON.toJSONString(digestValue));
+        System.out.println("provider.name   : " + provider.getName());
+        System.out.println("provider.version: " + provider.getVersion());
+        System.out.println("provider.info   : " + provider.getInfo());
 
     }
+
 }
--- src/main/java/com/deepblue/security_003_digeststream/DigestStreamTest.java
+++ src/main/java/com/deepblue/security_003_digeststream/DigestStreamTest.java
@@ -2,38 +2,48 @@ package com.deepblue.security_003_digeststream;
 
 import com.alibaba.fastjson.JSON;
 import com.deepblue.common.AlgorithmConstant;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.DigestInputStream;
+import java.security.DigestOutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
 
+/**
+ * 思考：
+ *      普通的
+ */
 public class DigestStreamTest {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        digestInputStreamTest();
+        digestOutputStreamTest();
+    }
 
+    private static void digestInputStreamTest() throws NoSuchAlgorithmException, IOException {
         MessageDigest digest = MessageDigest.getInstance(AlgorithmConstant.MD5.name());
-
         byte[] bytes = "MD5".getBytes();
         ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
         DigestInputStream dis = new DigestInputStream(bis, digest);
-//        dis.read(bytes, 0, bytes.length);
+
         dis.read(bytes);
-
-        byte[] digestBytes = digest.digest();
-        int digestLength = digest.getDigestLength();
-        String algorithm = digest.getAlgorithm();
-        Provider provider = digest.getProvider();
-
-        // bytes : "TUQ1", digestBytes : "aWkce9zDzm1dihNh8i0ErA=="
-        System.out.println("bytes        : " + JSON.toJSONString(bytes) + ", digestBytes : " + JSON.toJSONString(digestBytes));
-        System.out.println("digestLength : " + digestLength);
-        System.out.println("algorithm    : " + algorithm);
-        System.out.println("name : " + provider.getName() + ", version: " + provider.getVersion() + ", info: " + provider.getInfo());
-
+        byte[] disBytes = dis.getMessageDigest().digest();
+        System.out.println("disBytes:" + JSON.toJSONString(disBytes));
         dis.close();
+    }
 
+    public static void digestOutputStreamTest() throws NoSuchAlgorithmException, IOException {
+        byte[] bytes = "MD5".getBytes();
+        MessageDigest digest = MessageDigest.getInstance(AlgorithmConstant.MD5.name());
+        ByteOutputStream bos = new ByteOutputStream();
+        DigestOutputStream dos = new DigestOutputStream(bos, digest);
+        dos.write(bytes);
+        dos.flush();
+
+        byte[] dosBtyes = dos.getMessageDigest().digest();
+        System.out.println("dosBytes:" + JSON.toJSONString(dosBtyes));
+        dos.close();
     }
 }
