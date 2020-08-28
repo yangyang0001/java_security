0d4da8f714a567b48fe2870691885a347910fdb4
yangjianwei <wb-yjw637332@alibaba-inc.com>
2020-08-28 12:05:31 +0800

    '散列算法'

--- src/main/java/com/deepblue/inaction/study_007_mac_signature/HashTest.java
+++ src/main/java/com/deepblue/inaction/study_007_mac_signature/HashTest.java
@@ -1,6 +1,7 @@
 package com.deepblue.inaction.study_007_mac_signature;
 
 import com.alibaba.fastjson.JSON;
+import com.deepblue.common.AlgorithmConstant;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -9,23 +10,32 @@ import java.io.InputStream;
 import java.security.MessageDigest;
 import java.util.stream.Stream;
 
-public class MD5Test {
+/**
+ * 散列函数 输出值的长度都是固定的
+ */
+public class HashTest {
 
     public static void main(String[] args) throws IOException {
         String filePath1 = "D:\\study_workspace\\java_security\\src\\main\\java\\com\\deepblue\\inaction\\study_007_mac_signature\\mac.txt";
         String filePath2 = "D:\\study_workspace\\java_security\\src\\main\\java\\com\\deepblue\\inaction\\study_007_mac_signature\\mac_copy.txt";
+//        String algorithm = AlgorithmConstant.MD2.getName();    // 散列值长度: 16字节
+//        String algorithm = AlgorithmConstant.MD5.getName();    // 散列值长度: 16字节
+//        String algorithm = AlgorithmConstant.SHA1.getName();   // 散列值长度: 20字节
+        String algorithm = AlgorithmConstant.SHA256.getName(); // 散列值长度: 32字节
+//        String algorithm = AlgorithmConstant.SHA384.getName(); // 散列值长度: 48字节
+//        String algorithm = AlgorithmConstant.SHA512.getName(); // 散列值长度: 64字节
 
-        byte[] first  = getHashByFile(filePath1);
-        System.out.println(JSON.toJSONString(first));
+        System.out.println("algorithm :" + algorithm);
+        byte[] first  = getHashByFile(filePath1, algorithm);
+        System.out.println("first.length:" + first.length + ",  first:" + JSON.toJSONString(first));
         System.out.println("-----------------------------------------------------------------------");
 
-
-        byte[] second = getHashByFile(filePath2);
-        System.out.println(JSON.toJSONString(second));
+        byte[] second = getHashByFile(filePath2, algorithm);
+        System.out.println("second.length:" + first.length + ", second:" + JSON.toJSONString(second));
         System.out.println("-----------------------------------------------------------------------");
     }
 
-    public static byte[] getHashByFile(String filePath) {
+    public static byte[] getHashByFile(String filePath, String algorithm) {
         byte[] result = null;
         InputStream in = null;
         BufferedInputStream bis = null;
@@ -36,7 +46,7 @@ public class MD5Test {
             byte[] bytes = new byte[in.available()];
             bis.read(bytes);
             print(bytes);
-            MessageDigest digest = MessageDigest.getInstance("MD5");
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
             digest.update(bytes);
             result = digest.digest();
         } catch (Exception e) {
@@ -66,5 +76,6 @@ public class MD5Test {
         }
         System.out.println();
     }
+
 
 }
