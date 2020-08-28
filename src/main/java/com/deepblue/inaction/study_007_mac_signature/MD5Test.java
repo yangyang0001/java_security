c22d9b118b87d8ac50bce850cf548bcfd49ade46
yangjianwei <wb-yjw637332@alibaba-inc.com>
2020-08-28 10:11:23 +0800

    '消息指纹'

--- src/main/java/com/deepblue/inaction/study_007_mac_signature/MD5Test.java
+++ src/main/java/com/deepblue/inaction/study_007_mac_signature/MD5Test.java
@@ -7,82 +7,64 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
+import java.util.stream.Stream;
 
 public class MD5Test {
 
     public static void main(String[] args) throws IOException {
-        byte[] first = getFirstFileMessageDigestValue();
-        byte[] seond = getSeondFileMessageDigestValue();
-        System.out.println("first digest:" + JSON.toJSONString(first));
-        System.out.println("seond digest:" + JSON.toJSONString(seond));
+        String filePath1 = "D:\\study_workspace\\java_security\\src\\main\\java\\com\\deepblue\\inaction\\study_007_mac_signature\\mac.txt";
+        String filePath2 = "D:\\study_workspace\\java_security\\src\\main\\java\\com\\deepblue\\inaction\\study_007_mac_signature\\mac_copy.txt";
+
+        byte[] first  = getHashByFile(filePath1);
+        System.out.println(JSON.toJSONString(first));
+        System.out.println("-----------------------------------------------------------------------");
+
+
+        byte[] second = getHashByFile(filePath2);
+        System.out.println(JSON.toJSONString(second));
+        System.out.println("-----------------------------------------------------------------------");
     }
 
-    public static byte[] getFirstFileMessageDigestValue() {
+    public static byte[] getHashByFile(String filePath) {
         byte[] result = null;
-        InputStream  inputStream  = null;
+        InputStream in = null;
+        BufferedInputStream bis = null;
+
         try {
-            inputStream = new FileInputStream("D:\\study_workspace\\java_security\\src\\main\\java\\com\\deepblue\\inaction\\study_007_mac_signature\\mac.txt");
-            BufferedInputStream bis = new BufferedInputStream(inputStream);
-            byte[] bytes = new byte[inputStream.available()];
+            in = new FileInputStream(filePath);
+            bis = new BufferedInputStream(in);
+            byte[] bytes = new byte[in.available()];
             bis.read(bytes);
-            System.out.println("---------------------------------- first bytes start -----------------------------------");
-            for(byte b : bytes) {
-                System.out.print((char)b);
-            }
-            System.out.println("\n---------------------------------- first bytes end -----------------------------------");
-
+            print(bytes);
             MessageDigest digest = MessageDigest.getInstance("MD5");
-//            digest.update(bytes, 0, bytes.length);      //Ls3eOVkFHZE/YbFFeeoTbQ==
-            digest.update(bytes);                         //Ls3eOVkFHZE/YbFFeeoTbQ==
-
-            byte[] digestValue = digest.digest();
-            result = digestValue;
-
+            digest.update(bytes);
+            result = digest.digest();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
-                if(inputStream != null) {
-                    inputStream.close();
+                if(bis != null) {
+                    bis.close();
+                }
+                if(in != null) {
+                    in.close();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         return result;
     }
-    public static byte[] getSeondFileMessageDigestValue() {
-        byte[] result = null;
-        InputStream  inputStream  = null;
-        try {
-            inputStream = new FileInputStream("D:\\study_workspace\\java_security\\src\\main\\java\\com\\deepblue\\inaction\\study_007_mac_signature\\mac_copy.txt");
-            BufferedInputStream bis = new BufferedInputStream(inputStream);
-            byte[] bytes = new byte[inputStream.available()];
-            bis.read(bytes);
-            System.out.println("---------------------------------- seond bytes start -----------------------------------");
-            for(byte b : bytes) {
-                System.out.print((char)b);
-            }
-            System.out.println("\n---------------------------------- seond bytes end -----------------------------------");
 
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-//            digest.update(bytes, 0, bytes.length);      //Ls3eOVkFHZE/YbFFeeoTbQ==
-            digest.update(bytes);                         //Ls3eOVkFHZE/YbFFeeoTbQ==
-
-            byte[] digestValue = digest.digest();
-            result = digestValue;
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if(inputStream != null) {
-                    inputStream.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public static void print(byte[] bytes) {
+        if(bytes == null || bytes.length == 0) {
+            return;
         }
-        return result;
+        for (byte b : bytes) {
+            System.out.print((char)b);
+        }
+        System.out.println();
     }
 
 }
