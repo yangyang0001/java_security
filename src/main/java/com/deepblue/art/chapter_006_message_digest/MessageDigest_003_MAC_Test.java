e82f60493af5c68cf1fc8fe990a3c16c0874312f
yangjianwei <wb-yjw637332@alibaba-inc.com>
2020-09-25 19:35:16 +0800

    '消息摘要'

--- src/main/java/com/deepblue/art/chapter_006_message_digest/MessageDigest_003_MAC_Test.java
+++ src/main/java/com/deepblue/art/chapter_006_message_digest/MessageDigest_003_MAC_Test.java
@@ -1,4 +1,18 @@
 package com.deepblue.art.chapter_006_message_digest;
 
+import com.deepblue.common.AlgorithmConstant;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.HmacUtils;
+
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 public class MessageDigest_003_MAC_Test {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String algorithm = AlgorithmConstant.RSA.getName();
+        KeyPairGenerator generator = KeyPairGenerator.getInstance(algorithm);
+        generator.initialize(1024);
+
+    }
 }
